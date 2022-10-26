5409c589a46c735dca33ab11bd4a8aac21022ce3
riccardo conte <113691153+riccardoconte05@users.noreply.github.com>
2022-10-26 13:14:14 +0200

    Create es_b.c

---
+++ es_b.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main ()
+{
+	int num1,num2;
+	printf("inserisci il primo numero ");
+	scanf("%d",&num1);
+	printf("inserisci secondo numero ");
+	scanf("%d",&num2);
+	if
+
+	
+		(num1==num2)
+		printf("i due numeri sono uguali");
+	
+	else
+	
+	if 
+	(num1>num2)
+	
+	{
+	printf("il numero maggiore e' %d \n",num1);
+	printf("il numero minore e' %d",num2);		
+	}
+	
+	else 
+	{
+	
+	printf(" il numero maggiore e' %d",num2);
+	printf(" il numero minore e' %d", num1);
+	}	
+		
+	return 0;
+	
+	
+}
